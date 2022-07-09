04d5a0647c8fd9ae69ae55ee4be952585a0cf55a
a9500484054 <a9500484054@yandex.ru>
2022-07-09 11:56:09 +0300

    Внес исправления

--- js/app.js
+++ js/app.js
@@ -9,4 +9,35 @@ document.addEventListener('DOMContentLoaded', () => {
         menu.classList.toggle('menu--active')
     })
 
+
+    const whom = document.querySelector('.whom');
+    const blockTwoBtn = document.querySelectorAll('.block-two__btn');
+    const blockTwo = document.querySelector('.block-two');
+    const blockThree = document.querySelectorAll('.block-three');
+
+    if (whom) {
+        whom.addEventListener('click', (e) => {
+            if (e.target.classList.contains('block-two__btn')) {
+                const tabsPath = e.target.dataset.tabsPath;
+                blockTwoBtn.forEach(el => {el.classList.remove('block-two__btn--active')});
+                document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('block-two__btn--active');
+                umInvestorLpFeaturesHandler(tabsPath);
+            }
+            blockTwo.classList.add('block-two--active')
+        });
+    }
+    
+    const umInvestorLpFeaturesHandler = (path) => {
+        blockThree.forEach(el => {el.classList.remove('block-three--active')});
+        document.querySelector(`[data-tabs-target="${path}"]`).classList.add('block-three--active');
+    };
+
+    const blockThreeButton = document.querySelectorAll('.block-three__button');
+
+    blockThreeButton.forEach(el => {
+        el.addEventListener('click', () => {
+            blockThree.forEach(el => {el.classList.remove('block-three--active')});
+        })
+        
+    })
 });
