9df90a8a6377762dc99debfe8c243ead4a535e6c
a9500484054 <a9500484054@yandex.ru>
2022-07-11 09:09:12 +0300

    Внес исправления

--- js/app.js
+++ js/app.js
@@ -12,7 +12,6 @@ document.addEventListener('DOMContentLoaded', () => {
 
     const whom = document.querySelector('.whom');
     const blockTwoBtn = document.querySelectorAll('.block-two__btn');
-    const blockTwo = document.querySelector('.block-two');
     const blockThree = document.querySelectorAll('.block-three');
 
     if (whom) {
@@ -23,7 +22,6 @@ document.addEventListener('DOMContentLoaded', () => {
                 document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('block-two__btn--active');
                 umInvestorLpFeaturesHandler(tabsPath);
             }
-            blockTwo.classList.add('block-two--active')
         });
     }
     
@@ -38,6 +36,36 @@ document.addEventListener('DOMContentLoaded', () => {
         el.addEventListener('click', () => {
             blockThree.forEach(el => {el.classList.remove('block-three--active')});
         })
-        
     })
+
+
+    const startWrapper = document.querySelector('.start__wrapper');
+    const listElemItem = document.querySelectorAll('.list-elem__item');
+    const listTextItem = document.querySelectorAll('.list-text__item');
+
+    if (startWrapper) {
+        startWrapper.addEventListener('mouseover', (e) => {
+            if (e.target.classList.contains('list-elem__item')) {
+                const tabsPath = e.target.dataset.tabsPath;
+                listElemItem.forEach(el => {el.classList.remove('list-elem__item--active')});
+                document.querySelector(`[data-tabs-path="${tabsPath}"]`).classList.add('list-elem__item--active');
+                umInvestorFeaturesHandler(tabsPath);
+            }
+        });
+    }
+    
+    const umInvestorFeaturesHandler = (path) => {
+        listTextItem.forEach(el => {el.classList.remove('list-text__item--active')});
+        document.querySelector(`[data-tabs-target="${path}"]`).classList.add('list-text__item--active');
+    };
+
+    const enter = document.querySelectorAll('.enter');
+    const popup = document.querySelector('.popup-user');
+
+    enter.forEach((el) => {
+        el.addEventListener('click', (e) => {
+            popup.classList.add('popup-user--active')
+        });
+    });
 });
+
